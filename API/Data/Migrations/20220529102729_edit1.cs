012cc4bc46ff1e8af5201b27303e7193372f7968
jndheeraj <kaptanhook4u@gmail.com>
2022-05-31 14:48:38 +0530

    End of Section 4

---
+++ API/Data/Migrations/20220529102729_edit1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace API.Data.Migrations
+{
+    public partial class edit1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Passwordsalt",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Passwordsalt",
+                table: "Users");
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
+
+using API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
 
 namespace API
 {
@@ -34,13 +28,34 @@ namespace API
             //    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             //});
 
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseNpgsql(_config.GetConnectionString("DefaultConnection"));
-            });
+            //services.AddScoped<ITokenService, TokenService>();
+
+            //services.AddDbContext<DataContext>(options =>
+            //{
+            //    options.UseNpgsql(_config.GetConnectionString("DefaultConnection"));
+            //});
+
+            services.AddApplicationServices(_config);
+
             services.AddControllers();
 
             services.AddCors();
+
+            services.AddIdentityServices(_config);
+
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            //{
+            //    options.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
+            //        ValidateIssuer = false,
+            //        ValidateAudience = false
+
+            //    };
+            //});
+
+
 
         }
 
@@ -59,6 +74,8 @@ namespace API
             //app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200/"));
 
             app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
